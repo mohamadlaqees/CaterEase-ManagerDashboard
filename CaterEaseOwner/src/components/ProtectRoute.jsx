f7afab73229af2a161ece267a938ce753ba6d01a
mohamadlaqees <mohamadlk24@gmail.com>
2025-08-05 15:05:40 +0300

    delete redundant folder

--- CaterEaseOwner/src/components/ProtectRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router";
-const ProtectRoute = ({ children }) => {
-  const token = localStorage.getItem("authToken");
-  return token ? children : <Navigate to={"/login"} />;
-};
-
-export default ProtectRoute;
